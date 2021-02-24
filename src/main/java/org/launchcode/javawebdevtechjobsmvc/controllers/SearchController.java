d97bd03beea06c37ecba35ea6cfe0428d287d51c
booma1191 <booma1191.m@gmail.com>
2021-02-24 09:21:59 -0600

    Implemented the "View All" category

--- src/main/java/org/launchcode/javawebdevtechjobsmvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/javawebdevtechjobsmvc/controllers/SearchController.java
@@ -24,5 +24,10 @@ public class SearchController {
     }
 
     // TODO #3 - Create a handler to process a search request and render the updated search view.
+    @RequestMapping(value="results", method = {RequestMethod.GET, RequestMethod.POST})
+    @ResponseBody
+    public String displaySearchResults(String results) {
+        return results;
+    }//filler atm
 
 }
